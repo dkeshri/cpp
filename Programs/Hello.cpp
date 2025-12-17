3795541f9deb1d355db6bf6118b9f2aa72d2f28d
dkeshri <dkeshridev@gmail.com>
2025-12-17 20:02:02 +0530

    update

--- Programs/Hello.cpp
+++ Programs/Hello.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int a;
-    cout << "Enter a number: "; 
-    cin>>a;
-    cout << "You entered: " << a << endl;
-    cout << "Hello, World!" << endl;
-    return 0;
+    int n;
+    cout << "Enter numbers (Ctrl+D to end):" << endl;
+    while(cin >> n) {
+        cout << n << endl;
+    }
 }
