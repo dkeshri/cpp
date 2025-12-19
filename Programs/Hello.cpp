5051b42135d5312314ad24f6a3e13090dfab5775
dkeshri <dkeshridev@gmail.com>
2025-12-19 15:31:01 +0530

    update

--- Programs/Hello.cpp
+++ Programs/Hello.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int n;
-    while(cin >> n) {
-        cout << n << endl;
+    const int m=5,n=3;
+    int a[m][n];
+    for(int i=0;i<m;++i){
+        for(int j=0;j<n;++j){
+            cin>>a[i][j];
+        }
     }
+
+    for(int i=0;i<4;++i){
+        for(int j=0;j<3;++j){
+            cout<<a[i][j]<<" ";
+        }
+        cout<<endl;
+    }    
+    return 0;
 }
